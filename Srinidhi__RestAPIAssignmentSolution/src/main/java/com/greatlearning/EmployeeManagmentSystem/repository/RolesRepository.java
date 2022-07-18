f66c8e6503add8a2b5cd247b210070b9dfbdafc0
SrinidhiVenugopal <98889069+SrinidhiVenugopal@users.noreply.github.com>
2022-07-18 17:47:32 +0530

    Delete Srinidhi__RestAPIAssignmentSolution directory

--- Srinidhi__RestAPIAssignmentSolution/src/main/java/com/greatlearning/EmployeeManagmentSystem/repository/RolesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.EmployeeManagmentSystem.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.greatlearning.EmployeeManagmentSystem.entity.Role;
-
-public interface RolesRepository extends CrudRepository<Role, Integer> {
-
-}
